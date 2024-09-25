4ab8bb00e5ef6b5c9865abe58799f3abd2cf081f
Vadim Ivlev <vadimi.vlev@gmail.com>
2024-09-25 17:02:14 +0100

    fix: sprint2 exercises.

---
+++ sprints/2-simple-server/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"compress/gzip"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Server started at http://localhost:3000")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", defaultHandle)
+	http.ListenAndServe(":3000", gzipHandle(mux))
+}
+
+func defaultHandle(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	io.WriteString(w, "<html><body>"+strings.Repeat("Hello, world 200 times <br>", 200)+"</body></html>")
+}
+
+// gzipHandle - обработчик, который будет сжимать данные, и подключим его к серверу как middleware.
+func gzipHandle(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// проверяем, что клиент поддерживает gzip-сжатие;
+		// это упрощённый пример, в реальном приложении следует проверять все
+		// значения r.Header.Values("Accept-Encoding") и разбирать строку
+		// на составные части, чтобы избежать неожиданных результатов
+		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+			// если gzip не поддерживается, передаём управление
+			// дальше без изменений
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		// создаём gzip.Writer поверх текущего w
+		gz, err := gzip.NewWriterLevel(w, gzip.BestSpeed)
+		if err != nil {
+			io.WriteString(w, err.Error())
+			return
+		}
+		defer gz.Close()
+
+		w.Header().Set("Content-Encoding", "gzip")
+		// передаём обработчику страницы переменную типа gzipWriter для вывода данных
+		next.ServeHTTP(gzipWriter{ResponseWriter: w, Writer: gz}, r)
+	})
+}
+
+type gzipWriter struct {
+	http.ResponseWriter
+	Writer io.Writer
+}
+
+func (w gzipWriter) Write(b []byte) (int, error) {
+	// w.Writer будет отвечать за gzip-сжатие, поэтому пишем в него
+	return w.Writer.Write(b)
+}
